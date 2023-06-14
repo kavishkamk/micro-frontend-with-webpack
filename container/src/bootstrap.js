8dcd7066144a6e71b67d61cc1bf1d5f7b28dbafe
kavishkamk <kavishkamadhushan727@gmail.com>
2023-06-14 20:17:09 +0530

    test pipe line

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,6 @@
 import {mount as productMount} from "product/ProductIndex";
 import {mount as cartMount} from "cart/CartIndex";
 
-console.log("container");
+console.log("container..");
 productMount(document.querySelector("#my-products"));
 cartMount(document.querySelector("#my-cart"));
